ac3efecc9a57029ed24cb58917c0e917f4095390
Krishna Mohan Koyya <krishna@glarimy.com>
2018-06-12 09:01:57 +0530

    jdbc support

---
+++ glarimy-spring-jdbc/src/main/java/com/glarimy/library/data/LibraryStore.java
@@ -0,0 +1,31 @@
+package com.glarimy.library.data;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+
+import com.glarimy.library.api.Book;
+
+public class LibraryStore extends JdbcDaoSupport {
+	public void create(Book book) {
+		getJdbcTemplate().update("INSERT INTO library VALUES(?, ?)", new Object[] { book.getIsbn(), book.getTitle() });
+	}
+
+	public Book find(int isbn) {
+		List<Book> list = getJdbcTemplate().query("SELECT * FROM library where isbn=?", new Object[] { isbn },
+				new RowMapper<Book>() {
+					public Book mapRow(ResultSet rs, int rowNo) throws SQLException {
+						Book book = new Book();
+						book.setIsbn(rs.getInt("isbn"));
+						book.setTitle(rs.getString("title"));
+						return book;
+					}
+				});
+		if (list.size() != 0)
+			return list.get(0);
+		return null;
+	}
+}
---
+++ glarimy-spring-jdbc/src/main/java/com/glarimy/library/ui/LibraryConsole.java
@@ -0,0 +1,24 @@
+package com.glarimy.library.ui;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.glarimy.library.api.Book;
+import com.glarimy.library.api.BookNotFoundExcetion;
+import com.glarimy.library.api.Library;
+
+public class LibraryConsole {
+	public static void main(String[] args) throws Exception {
+		ApplicationContext context = new ClassPathXmlApplicationContext("jdbc.xml");
+		Library library = (Library) context.getBean("library");
+		try {
+			library.add(new Book(1234, "Spring JDBC"));
+			Book book = library.find(1234);
+			System.out.println(book.getTitle());
+		} catch (BookNotFoundExcetion bnfe) {
+			System.out.println("Sorry ... not found!");
+		} catch (Exception e) {
+			System.out.println("Internal error!");
+		}
+	}
+}
